f937a3cf5fded92c153e24f5a89437e0a14a8f63
Miha Stele <stele1000@gmail.com>
2024-05-17 20:02:50 +0000

    Celsius

--- celsius_to_fahrenheit.c
+++ celsius_to_fahrenheit.c
@@ -10,12 +10,31 @@ int main()
     step = 20;
 
     fahr = lower;
+    printf("Fahrenheit    Celsius\n");
     while (fahr <= upper)
     {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
+        printf("%10.0f   %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 
+    printf("Celsius    Fahrenheit");
+    celsius = lower;
+    while (celsius <= upper)
+    {
+        fahr = (9.0 / 5.0) * celsius + 32.0;
+        printf("%10.0f   %6.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+
     return 0;
+}
+
+void option2()
+{
+    int fahr = 0;
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    {
+        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    }
 }
